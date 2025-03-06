be8425a4683bb147e58c104d2eb95a8a92197cfd
Daniil Dron <dron@element-soft.com>
2025-03-06 15:56:51 +0300

    add mongoDB

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import {registerValidator} from './validations/auth';
 import { validationResult } from 'express-validator';
 
+import {registerValidator} from './validations/auth.js';
+import UserModel from './models/User.js';
 
 
-mongoose.connect('mongodb+srv://admin:123@cluster0.k7kmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://admin:123@cluster0.k7kmp.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => console.log('DB ok'))
 .catch((err) => console.log('DB error', err));
 
@@ -18,15 +20,26 @@ app.get('/', (req, res) => {
     res.send('WebApp is working!')
 })
 
-app.post('/auth/register', registerValidator, (req, res) => {
+app.post('/auth/register', registerValidator, async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json(errors.array())
     }
 
-    res.json({
-        success: true,
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        passwordHash,
+        avatarUrl: req.body.avatarUrl,
     });
+
+    const user = await doc.save();
+
+    res.json(user);
 });
 
 app.listen(4444, (err) => {
