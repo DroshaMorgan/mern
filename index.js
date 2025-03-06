dc34fa7189d0df35f646519b2afe96aab7164625
Daniil Dron <dron@element-soft.com>
2025-03-06 16:13:05 +0300

    auth user

--- index.js
+++ index.js
@@ -21,25 +21,46 @@ app.get('/', (req, res) => {
 })
 
 app.post('/auth/register', registerValidator, async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array())
+    try {
+        // Валидация запроса
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+    
+        // Шифруем хэш пароль
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+    
+        // Создаем документ для БД
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            passwordHash: hash,
+            avatarUrl: req.body.avatarUrl,
+        });
+    
+        const user = await doc.save();
+
+        const token = jwt.sign({
+                _id: user._id,
+            }, 
+            'secret123',
+            {
+               expiresIn: '30d',
+            }
+        )
+    
+    const {passwordHash, ...userData} = user._doc;
+
+        res.json({...userData, token});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегестрироваться!'
+        });
     }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        passwordHash,
-        avatarUrl: req.body.avatarUrl,
-    });
-
-    const user = await doc.save();
-
-    res.json(user);
 });
 
 app.listen(4444, (err) => {
